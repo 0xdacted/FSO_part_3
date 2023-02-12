a6e1fb5a2e59cdd96569a3379c64bc70c03d3fec
Red <crabmarket56@gmail.com>
2023-02-12 09:27:22 +0900

    Add view persons functionality

--- mongo.js
+++ mongo.js
@@ -5,6 +5,7 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 
+
 const password = process.argv[2]
 const url = `mongodb+srv://phonebook:${password}@cluster0.w5h7edt.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
@@ -18,12 +19,22 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
+if (process.argv.length < 4) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    process.exit(1)
+  })
+} else {
 const person = new Person({
   name: process.argv[3],
   number: process.argv[4],
 })
+  person.save().then(result => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
 
-person.save().then(result => {
-  console.log(`added ${person.name} number ${person.number} to phonebook`)
-  mongoose.connection.close()
 })
+}
+
