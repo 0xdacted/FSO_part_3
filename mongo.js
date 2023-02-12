fb65a151988fdced84c03e05db594d1d57568711
Red <crabmarket56@gmail.com>
2023-02-12 09:10:47 +0900

    Add person definitions

--- mongo.js
+++ mongo.js
@@ -12,16 +12,18 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  
+  name: String,
+  number: Number, 
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-  
+  name: process.argv[3],
+  number: process.argv[4],
 })
 
 person.save().then(result => {
-  console.log('person saved')
+  console.log(`added ${person.name} number ${person.number} to phonebook`)
   mongoose.connection.close()
 })
